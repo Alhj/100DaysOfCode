859066cb1c5bd8eb8b70fdf638ae78e7058c487a
Alhj <alex_hjortenkrans@outlook.com>
2021-10-07 20:20:58 +0200

    working on a storefront

---
+++ store_front/src/components/header/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="inline">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Header'
+
+}
+</script>
+
+
+<style scoped>
+.header {
+  display: inline;
+}
+</style>
